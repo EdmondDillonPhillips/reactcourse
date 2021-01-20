d9aa67aa9ebe864a0694ef0e3a5d5a22c7361fb1
Dillon Phillips <edmond.dillon.phillips@gmail.com>
2021-01-19 22:07:49 -0500

    End Of First Lesson, Learned about JSX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 function App() {
-  return "Hello World";
+  return (
+  <div>
+    <button className="btn">-</button>
+    <span>0</span>
+    <button className="btn">+</button>
+  </div>
+  
+  );
 }
 
 export default App;
