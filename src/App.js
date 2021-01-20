57961f8d2e703bb86c55ba7415affe01f402b508
Dillon Phillips <edmond.dillon.phillips@gmail.com>
2021-01-20 10:46:12 -0500

    passing props from Recipie List to Recipie Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { useState } from 'react';
+import RecipieList from './RecipeList'
 
 
 function App() {
   return (
     <>
-    null
+    <RecipieList recipies={sampleRecipies}/>
     </>
   );
 }
+
+const sampleRecipies = [
+  {
+    id: 1,
+    name: 'Plain Chicken',
+    servings: 3,
+    cookTime: '1:45',
+    instrucitons: '1. Put salt on Chicken\n 2. Put Chicken in oven\n 3. Eat chicken'
+  },
+  {
+    id: 2,
+    name: 'Plain Pork',
+    servings: 4,
+    cookTime: '2:55',
+    instrucitons: '1. Put paprika on Pork\n 2. Put Pork in oven\n 3. Eat Pork'
+  }
+]
 
 export default App;
--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Recipie from './Recipe'
 
-export default function RecipeList() {
+export default function RecipeList({recipies}) {
     return (
-        <>
-            
-        </>
+        <div>
+            {
+                recipies.map( recipie => {
+                    return <Recipie key={recipie.id} {...recipie}/>
+                })
+            }
+        </div>
+       
     )
 }
